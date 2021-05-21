e7af8ae2f742001d01ced1b41986fb4de158a7fc
Serverguyken <serverguyken@gmail.com>
2021-05-21 16:11:46 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
-createApp(App).mount("#app");
+import './assets/css/build.css';
+import './assets/css/custom.css';
+import router from './router/routes';
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ src/router/routes.js
@@ -0,0 +1,70 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Contact from "../views/Contact.vue";
+import Features from "../views/Home.vue";
+import Pricing from "../views/Pricing.vue";
+import NotFound from "../views/NotFound.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      title: "LogSnap | Issue Tracking"
+    }
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+    meta: {
+      title: "About | LogSnap"
+    }
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
+    meta: {
+      title: "Contact | LogSnap"
+    }
+  },
+  {
+    path: "/features",
+    name: "Features",
+    component: Features,
+    meta: {
+      title: "Features | LogSnap"
+    }
+  },
+  {
+    path: "/pricing",
+    name: "Pricing",
+    component: Pricing,
+    meta: {
+      title: "Pricing | LogSnap"
+    }
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    component: NotFound,
+    meta: {
+      title: "404 | Not Found"
+    }
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  linkActiveClass: 'is-active',
+  linkExactActiveClass: 'exact-active',
+  routes,
+});
+
+router.afterEach(to => {
+  document.title = to.meta.title;
+});
+
+export default router;
